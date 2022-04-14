a93aad96e5db9249cd43503a1cf0bda4c0c5dc64
Matthias Colin <matthias.colin@sorbonne-nouvelle.fr>
2022-04-14 13:34:47 +0200

    correction

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,20 +1,28 @@
 import HeaderHome from "./HeaderHome";
 import "./Home.css";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { states } from "./data";
 
 function Home(props) {
-    const [firstName, setFirstName] = useState();
-    const [lastName, setLastName] = useState();
+    // const [firstName, setFirstName] = useState();
+    // const [lastName, setLastName] = useState();
     const [startDateBirth, setStartDateBirth] = useState(new Date());
     const [startDate, setStartDate] = useState(new Date());
-    const [street, setStreet] = useState();
-    const [city, setCity] = useState();
-    const [stateUS, setStateUS] = useState();
-    const [zip, setZip] = useState();
-    const [department, setDepartment] = useState();
+    // const [street, setStreet] = useState();
+    // const [city, setCity] = useState();
+    // const [stateUS, setStateUS] = useState();
+    // const [zip, setZip] = useState();
+    // const [department, setDepartment] = useState();
+
+    const inputFirstName = useRef(null);
+    const inputLastName = useRef(null);
+    const inputStreet = useRef(null);
+    const inputCity = useRef(null);
+    const inputStateUS = useRef(null);
+    const inputZip = useRef(null);
+    const inputDepartment = useRef(null);
 
     const handleSumbit = (e) => {
         e.preventDefault();
@@ -29,16 +37,28 @@ function Home(props) {
         // console.log(department);
 
         const form = {
-            firstName,
-            lastName,
-            startDateBirth,
-            startDate,
-            street,
-            city,
-            stateUS,
-            zip,
-            department
+            firstName: inputFirstName.current.value,
+            lastName: inputLastName.current.value,
+            startDateBirth: startDateBirth,
+            startDate: startDate,
+            street: inputStreet.current.value,
+            city: inputCity.current.value,
+            stateUS: inputStateUS.current.value,
+            zip: inputZip.current.value,
+            department: inputDepartment.current.value,
         };
+        // const form = {
+        //     inputFirstName,
+        //     lastName,
+        //     startDateBirth,
+        //     startDate,
+        //     street,
+        //     city,
+        //     stateUS,
+        //     zip,
+        //     department
+        // };
+        console.log(form);
         props.setEmployees(form);
     }
 
@@ -47,10 +67,10 @@ function Home(props) {
             <HeaderHome />
             <form className="home-form" onSubmit={handleSumbit}>
                 <label htmlFor="first-name">First Name</label>
-                <input type="text" id="first-name" onChange={(value) => setFirstName(value.target.value)} />
+                <input type="text" id="first-name" ref={inputFirstName} />
 
                 <label htmlFor="last-name">Last Name</label>
-                <input type="text" id="last-name" onChange={(value) => setLastName(value.target.value)} />
+                <input type="text" id="last-name" ref={inputLastName} />
 
                 <label htmlFor="date-of-birth">Date of Birth</label>
                 <DatePicker selected={startDateBirth} onChange={(date) => setStartDateBirth(date.target.value)} />
@@ -62,13 +82,13 @@ function Home(props) {
                     <legend>Address</legend>
 
                     <label htmlFor="street">Street</label>
-                    <input id="street" type="text" onChange={(value) => setStreet(value.target.value)} />
+                    <input id="street" type="text" ref={inputStreet} />
 
                     <label htmlFor="city">City</label>
-                    <input id="city" type="text" onChange={(value) => setCity(value.target.value)} />
+                    <input id="city" type="text" ref={inputCity} />
 
                     <label htmlFor="state">State</label>
-                    <select name="state" id="state" onChange={(value) => setStateUS(value.target.value)} defaultValue={"default"}>
+                    <select name="state" id="state" ref={inputStateUS} defaultValue={"default"}>
                         <option value={"default"} disabled>
                             Choose an option
                         </option>
@@ -78,10 +98,10 @@ function Home(props) {
                     </select>
 
                     <label htmlFor="zip-code">Zip Code</label>
-                    <input id="zip-code" type="number" onChange={(value) => setZip(value.target.value)} />
+                    <input id="zip-code" type="number" ref={inputZip} />
                 </fieldset>
                 <label htmlFor="department">Department</label>
-                <select name="department" id="department" onChange={(value) => setDepartment(value.target.value)} defaultValue={"default"}>
+                <select name="department" id="department" ref={inputDepartment} defaultValue={"default"}>
                     {/* // TODO : Bug car il faut mettre un default je crois sinon une erreur sur la console.log */}
                     <option value={"default"} disabled>
                         Choose an option
